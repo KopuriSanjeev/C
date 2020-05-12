d1ad5377d2349a362f4dfe3f3cb6d2280c97783b
KopuriSanjeev <35334095+KopuriSanjeev@users.noreply.github.com>
2020-05-12 18:54:46 +0530

    C Program with out main
    C Program with out main

---
+++ programs/typical/CprogramWithOutmain.c
@@ -0,0 +1,11 @@
+#include<stdio.h>    
+#define start main 
+ 
+// C Program without main() function
+// We can write c program without using main() function. To do so, we need to use #define preprocessor directive.
+
+// Let's see a simple program to print "hello" without main() function.
+
+void start() {    
+   printf("Hello");    
+}   
