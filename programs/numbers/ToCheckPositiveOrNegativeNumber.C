c3b95d3647e844beb595c571225050ef874b794c
KopuriSanjeev <35334095+KopuriSanjeev@users.noreply.github.com>
2020-08-28 20:11:14 +0530

    Create ToCheckPositiveOrNegativeNumber.C

---
+++ programs/numbers/ToCheckPositiveOrNegativeNumber.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	
+	double n = -12.5;
+	
+	if(n == 0)
+	    printf("Number is neither positive nor negavite");
+	else if(n>0)
+	    printf("Number is positive");
+	else
+	    printf("Number is negavite");
+	return 0;
+}
+
